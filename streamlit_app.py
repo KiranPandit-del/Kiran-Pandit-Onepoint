7441362cdf7ac5346ccf1545c0cc6e1bf3feea87
KiranPandit-del <kiran.pandit@onepointltd.com>
2024-08-13 12:31:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,5 +54,5 @@ if ingredients_list:
 #New section to display fruit juice
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
     
