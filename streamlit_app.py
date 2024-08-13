a1d9644d2bb415c05861ff7e5d7b34b7b1ffcf68
KiranPandit-del <kiran.pandit@onepointltd.com>
2024-08-13 12:08:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 # Write directly to the app
-streamlit.title('My Parents New HEalty Diner')
+st.title('My Parents New HEalty Diner')
 #st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want is your custom Smoothies!
